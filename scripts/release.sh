794a7f15cb2512dc7de2001a479a8dce48ef0f4f
Roman Kuba <roman.kuba@gmail.com>
2018-12-07 12:08:57 +0100

    Release script

---
+++ scripts/release.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+if [[ -z $1 ]]; then
+  CURRENT_VERSION=$(node -p -e "require('./package.json').version")
+  echo "Enter new version: (currently $CURRENT_VERSION)"
+  read -r VERSION
+else
+  VERSION=$1
+fi
+
+read -p "Releasing $VERSION - are you sure? (y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  npm version "$VERSION"
+  npm publish --otp=$(op get totp npm)
+else
+    echo 'Release aborted!'
+fi
+./\''//\
